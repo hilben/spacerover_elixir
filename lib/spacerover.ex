26d481d8e4fbe16d925f7e3a7d0a641c8c01e903
Benjamin Hiltpolt <benjamin.hiltpolt@xing.com>
2016-11-09 10:44:40 +0100

    Some improvements

--- lib/spacerover.ex
+++ lib/spacerover.ex
@@ -1,16 +1,12 @@
 require IEx;
 
 defmodule Spacerover do
-
     def main(args) do
       args |> parse_args |> process
     end
 
-    def process([]) do
-      IO.puts "No arguments given"
-    end
-
-    def process(options) do
+    defp process([]), do: IO.puts "No arguments given"
+    defp process(options) do
       IO.puts "Parsing #{options[:file]}"
       {_, input} =  File.read options[:file]
 
@@ -24,33 +20,31 @@ defmodule Spacerover do
     end
 
   defmodule InputProcessor do
-    def input() do
-      "5 5
-      1 2 N
-      LMLMLMLMM
-      3 3 E
-      MMRMMRMRRM" |> String.split("\n")
-    end
-
+    def process(input) when is_bitstring(input), do: process(String.split(input,"\n"))
     def process([plateau | t]) do
       plateau |> process_rover_input(t, [])
     end
 
     def process_rover_input(_, [""|_], state), do: state
     def process_rover_input(plateau, [position | t], state) do
-      {x,y,a} = position |> String.trim |> String.split |> List.to_tuple
-      position = {String.to_integer(x), String.to_integer(y), a}
+      position = position |> process_position
 
       [movement | remainder] = t
 
-      updated = movement |>
-      String.trim |> String.split("") |> 
-      Enum.reduce(position, fn (move, position) -> updatemove(position,{move}) |> IO.inspect(limit: 1) end)
-
-      IO.puts("updated")
-      IO.inspect(updated)
+      updated = movement 
+                |> String.trim 
+                |> String.split("") 
+                |> Enum.reduce(position, fn (move, position) -> updatemove(position,{move}) end)
 
       process_rover_input(plateau, remainder, state ++ [updated])
+    end
+
+    def process_position(position) do
+      {x,y,direction} = position 
+      |> String.trim 
+      |> String.split 
+      |> List.to_tuple
+       {String.to_integer(x), String.to_integer(y), direction}
     end
 
     def process_rover_input(_, _, state), do: state
--- test/spacerover_test.exs
+++ test/spacerover_test.exs
@@ -3,8 +3,14 @@ defmodule SpaceroverTest do
   doctest Spacerover
 
   test "spacerover is working" do
+    input = 
+    "5 5
+    1 2 N
+    LMLMLMLMM
+    3 3 E
+    MMRMMRMRRM"
+
     output = [{1, 1, "N"}, {5, 5, "E"}]
-    input = Spacerover.InputProcessor.input()  
     assert Spacerover.InputProcessor.process(input) ==  output
   end
 end
